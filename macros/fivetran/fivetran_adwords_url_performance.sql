d8e8b718efb51d01e8fe91a964bc1ba601d4b1ec
Jess Williams <jess@fishtownanalytics.com>
2020-03-23 12:51:46 -0500

    update with updated column names from fivetran connector

--- macros/fivetran/fivetran_adwords_url_performance.sql
+++ macros/fivetran/fivetran_adwords_url_performance.sql
@@ -18,30 +18,30 @@ aggregated as (
     select
 
         {{ dbt_utils.surrogate_key (
-            'customerid',
-            'finalurl',
-            'day',
-            'campaignid',
-            'adgroupid'
+            'customer_id',
+            'final_urls',
+            'date',
+            'campaign_id',
+            'ad_group_id'
             
         ) }}::varchar as id,
 
-        day::date as date_day,
+        date::date as date_day,
 
-        {{ dbt_utils.split_part('finalurl', "'?'", 1) }} as base_url,
-        {{ dbt_utils.get_url_host('finalurl') }} as url_host,
-        '/' || {{ dbt_utils.get_url_path('finalurl') }} as url_path,
-        {{ dbt_utils.get_url_parameter('finalurl', 'utm_source') }} as utm_source,
-        {{ dbt_utils.get_url_parameter('finalurl', 'utm_medium') }} as utm_medium,
-        {{ dbt_utils.get_url_parameter('finalurl', 'utm_campaign') }} as utm_campaign,
-        {{ dbt_utils.get_url_parameter('finalurl', 'utm_content') }} as utm_content,
-        {{ dbt_utils.get_url_parameter('finalurl', 'utm_term') }} as utm_term,
-        campaignid as campaign_id,
-        campaign as campaign_name,
-        adgroupid as ad_group_id,
-        adgroup as ad_group_name,
-        customerid as customer_id,
-        _sdc_report_datetime,
+        {{ dbt_utils.split_part('final_urls', "'?'", 1) }} as base_url,
+        {{ dbt_utils.get_url_host('final_urls') }} as url_host,
+        '/' || {{ dbt_utils.get_url_path('final_urls') }} as url_path,
+        {{ dbt_utils.get_url_parameter('final_urls', 'utm_source') }} as utm_source,
+        {{ dbt_utils.get_url_parameter('final_urls', 'utm_medium') }} as utm_medium,
+        {{ dbt_utils.get_url_parameter('final_urls', 'utm_campaign') }} as utm_campaign,
+        {{ dbt_utils.get_url_parameter('final_urls', 'utm_content') }} as utm_content,
+        {{ dbt_utils.get_url_parameter('final_urls', 'utm_term') }} as utm_term,
+        campaign_id,
+        campaign_name,
+        ad_group_id,
+        ad_group_name,
+        customer_id,
+        _fivetran_synced as fivetran_synced_at,
 
         sum(clicks) as clicks,
         sum(impressions) as impressions,
@@ -59,7 +59,7 @@ ranked as (
     
         *,
         rank() over (partition by id
-            order by _sdc_report_datetime desc) as latest
+            order by fivetran_synced_at desc) as latest
             
     from aggregated
 
